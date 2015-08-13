9cd21a8449020c2cfd683bae99c4b339279c8b5a
Sandesh Gade <sandeshgade@gmail.com>
2015-08-13 15:30:39 +0530

    Test Code for conversion of multi-line FASTA files to single-line FASTA file.
    Features:
    ---- Fast and silent conversion.
    
    Bugs:
    ---- None so far.
    
    Conclusion:
    ---- Safe for use for the Dataset Cleaner Module v2.

--- Test.py
+++ Test.py
@@ -1,19 +1,61 @@
-__author__ = 'cyberbeast'
+# __author__ = 'cyberbeast'
+# import os as os
+# from Bio import SeqIO, Seq
+# from multiprocessing import Pool, Process, Manager
+# from py2neo import neo4j, Node, Relationship, watch, authenticate, Graph
+# import glob
+# import subprocess
+#
+#
+# def clean(file_name):
+#     # print(str(os.getcwd()) + '/GenomeDataset/Chromosomes/' + file_name)
+#     # print(os.path.basename(file_name))
+#     # p = subprocess.Popen(["echo", str(os.getcwd())])
+#     p = subprocess.Popen(["fasta_formatter", "-i", file_name, "-o", os.path.basename(file_name) + str("_TEST")], stdout=subprocess.PIPE)
+#     (output, err) = p.communicate()
+#     print(output)
+#
+# if __name__ == "__main__":
+#
+#     filelist = []
+#     # use glob module
+#     for name in glob.glob(str(os.getcwd()) + '/GenomeDataset/Chromosomes/*.fa'):
+#         filelist.append(name)
+#
+#     clean(filelist[0])
 
-from itertools import product, chain
-from multiprocessing import Pool
-from py2neo import neo4j, Node, Relationship, watch, authenticate, Graph
+import sys
+from Bio import SeqIO
 
-if __name__ == '__main__':
-    watch("httpstream")
-    # watch("py2neo.cypher")
-    authenticate("192.168.6.74:7474", "neo4j", "trvlr")
-    graph = neo4j.Graph("http://192.168.6.64:7474/db/data/")
+def sequence_cleaner(fasta_file,min_length=0,por_n=100):
+    #create our hash table to add the sequences
+    sequences={}
 
-    record = graph.cypher.execute("MATCH (n:DataFile) WHERE n.ID = {name} RETURN n.SequenceCount")
-    if record is None:
-        print("None")
-    else:
-        print("Huh?")
+    #Using the biopython fasta parse we can read our fasta input
+    for seq_record in SeqIO.parse(fasta_file, "fasta"):
+        #Take the current sequence
+        sequence=str(seq_record.seq).upper()
+        #Check if the current sequence is according to the user parameters
+        if (len(sequence)>=min_length and (float(sequence.count("N"))/float(len(sequence)))*100<=por_n):
+       # If the sequence passed in the test "is It clean?" and It isnt in the hash table , the sequence and Its id are going to be in the hash
+            if sequence not in sequences:
+                sequences[sequence]=seq_record.id
+       #If It is already in the hash table, We're just gonna concatenate the ID of the current sequence to another one that is already in the hash table
+            else:
+                sequences[sequence]+="_"+seq_record.id
 
-    print(record.one)
+
+    #Write the clean sequences
+
+    #Create a file in the same directory where you ran this script
+    output_file=open("clear_"+fasta_file,"w+")
+    #Just Read the Hash Table and write on the file as a fasta format
+    for sequence in sequences:
+            output_file.write(">"+sequences[sequence]+"\n"+sequence+"\n")
+    output_file.close()
+
+    print("CLEAN!!!\nPlease check clear_"+fasta_file)
+
+
+sequence_cleaner("hs_ref_GRCh38_chr1.fa")
+ 
