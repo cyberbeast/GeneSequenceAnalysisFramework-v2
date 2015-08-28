f58b0edb104cd0e67e70a16dfa87a51aeaea86b7
cyberbeast <sandeshgade@gmail.com>
2015-08-28 11:21:34 +0530

    TEST BUILD ONLY-------- Distributed Unique Pattern Generation

--- d_process_task.py
+++ d_process_task.py
@@ -42,7 +42,7 @@ def process(filename):
 
 @app.task
 def unique_pattern_generation(depth):
-	print("I am doing - " + depth)
+	print("I am doing - " + str(depth))
 	temp_result = []
 
 	if depth == 1:
