73c02c382931053581a6bc2ffd6d4b170d2f2c88
Sandesh Gade <sandeshgade@gmail.com>
2015-09-08 08:55:39 +0530

    Old Changes

--- tree_compressor.py
+++ tree_compressor.py
@@ -4,22 +4,31 @@ from itertools import product
 import pprint
 import glob
 import numpy as np
+import os
+import re
+
+_nsre = re.compile('([0-9]+)')
+
+def natural_sort_key(s):
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(_nsre, s)]
+
 
 filelist = []
 for name in glob.glob('GenomeDataset/Processing/*pTREE'):
-	filelist.append(name)
-
-pattern_list = ["".join(x) for i in range(1, 5) for x in product(* ['ATGC']* i)]
+    filelist.append(name)
+filelist.sort(key=natural_sort_key)
+pattern_list = ["".join(x) for i in range(1, 5) for x in product(*['ATGC'] * i)]
 pattern_list.sort()
 count_matrix = {}
 result = {pattern: [] for pattern in pattern_list}
 
 for infile in filelist:
-	with open(infile, 'rb') as in_fh:
-		new_tree = pickle.load(in_fh)
+    with open(infile, 'rb') as in_fh:
+        new_tree = pickle.load(in_fh)
 
-	for pattern in pattern_list:
-		result[pattern].append(new_tree.count(pattern))
+    for pattern in pattern_list:
+        # result[pattern].append(os.path.basename(infile) + "-->" + str(new_tree.count(pattern)))
+        result[pattern].append(new_tree.count(pattern))
 
 pprint.pprint(result, width=250)
 #
@@ -30,4 +39,4 @@ pprint.pprint(result, width=250)
 input_str = str(input('PATTERN? \t'))
 input_chr = int(input('CHR? \t\t'))
 
-print(str(result[input_str][input_chr]))
+print(str(result[input_str][input_chr - 1]))
