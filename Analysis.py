53f38595322e3ff130abcf64f7beeaf6ccd88b75
Sandesh Gade <sandeshgade@gmail.com>
2015-09-18 09:26:39 +0530

    Stable Commit. (18/09/2015) -------------------------------------------------------------------------------------------------------- * New visualization code added in the Visualization script * Commented Code for Analysis and Visualization scripts.

--- Analysis.py
+++ Analysis.py
@@ -32,12 +32,6 @@ def plot1():
     ax.set_zlabel("Chromosome")
 
     ax.set_zlim3d(1, 22)
-
-    s = 50
-    fig, ax = plt.subplots()
-    ax.scatter(y_val_real, y_val_imag, s, 'r', marker='o')
-    ax.set_xlabel("Real")
-    ax.set_ylabel("Imaginary")
     plt.show()
 
 for name in glob.glob('GenomeDataset/Processing/*pChromosome'):
@@ -47,7 +41,11 @@ if len(filelist) == 0:
     for name in glob.glob('GenomeDataset/Processing/*pTREE'):
         filelist.append(name)
 
-df1 = pd.DataFrame()
+
+df_vis1 = pd.DataFrame(columns=('Real', 'Imaginary'))
+df_vis2 = pd.DataFrame()
+
+rows = []
 
 i = 1
 with open('Visualizer/output-file.txt', 'w') as outf:
@@ -65,25 +63,40 @@ with open('Visualizer/output-file.txt', 'w') as outf:
         y_val_imag.append(y_val.imag)
         x_val.append([i for _ in range(len(y_val))])
 
-        y_r = [y.real for y in y_val.tolist()]
-        y_i = [y.imag for y in y_val.tolist()]
+        y_r = [y.real for y in y_val.tolist()]  # List of all real values from EV calculation for current chromosome
+        y_i = [y.imag for y in y_val.tolist()]  # List of all imaginary values from EV calculation for current chromosome
 
+        # Populate intermediate list to store Analysis 1 DataFrame data
         for rr, ri in zip(y_r, y_i):
-            outf.write(str(rr) + "," + str(ri) + '\n')
+            rows.append((str(rr), str(ri)))
 
         y_r_square = [real**2 for real in y_r]
         y_i_square = [imag**2 for imag in y_i]
 
+        # Define and populate list of data for Analysis 2 DataFrame
         y_sqrt_of_sum_of_R_I_list = []
 
         for rr2, ri2 in zip(y_r_square, y_i_square):
             y_sqrt_of_sum_of_R_I_list.append(np.sqrt(rr2 + ri2))
 
-        df1["C" + str(i)] = y_sqrt_of_sum_of_R_I_list
+        # Populate Analysis 2 DataFrame with current chromosome
+        df_vis2["C" + str(i)] = y_sqrt_of_sum_of_R_I_list
 
         i += 1
 
-df1.to_csv('Visualizer/squareroot-of-sum-of-squares-of-real-and-imaginary.csv')
-plot1()
+# Analysis 1 DataFrame Operations
+for row in rows:
+    df_vis1.loc[len(df_vis1)] = row
+
+df_vis1.to_csv('Visualizer/real-vs-imaginary.csv')
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Analysis 2 DataFrame Operations
+df_vis2.to_csv('Visualizer/squareroot-of-sum-of-squares-of-real-and-imaginary.csv')
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Uncomment next line for 3D plot of preliminary results
+# plot1()
+
 
 
--- Visualizer/Vis2.py
+++ Visualizer/Vis2.py
@@ -1,16 +1,38 @@
 __author__ = 'cyberbeast'
 import pandas as pd
+from bokeh.plotting import figure
 from bokeh.charts import Line, show, output_file
 from bokeh.models.widgets import Panel, Tabs
 
-data_coll = pd.read_csv(open("squareroot-of-sum-of-squares-of-real-and-imaginary.csv"),)
-data = pd.DataFrame(dict(
-    p=data_coll['C1']
-))
-output_file("Visualizations.html")
-tab1_tools = "pan, wheel_zoom, box_zoom, reset, save, hover, crosshair, tap, poly_select, lasso_select, resize"
-p = Line(data_coll, title="Magnitude vs. Eigen Position", xlabel='Position in Eigen Value Set', ylabel='Magnitude', width=1500, height=850, legend=True, tools=tab1_tools)
-tab1 = Panel(child=p, title="sqrt(x^2+y^2)")
+# Common settings for all visualizations
+output_file("Visualizations.html", title="GSAF 2.0 Results")
+vis_common_width = 1024
+vis_common_height = 768
+vis_common_tab_tools = "pan, wheel_zoom, box_zoom, reset, save, crosshair, tap, poly_select, resize"
 
-tabs = Tabs(tabs=[tab1])
-show(tabs)
+# Load data for visualizations
+data_collection_vis1 = pd.read_csv(open("real-vs-imaginary.csv"),)
+data_collection_vis1_formatted = pd.DataFrame(dict(
+    x=data_collection_vis1['Real'],
+    y=data_collection_vis1['Imaginary']
+))
+data_collection_vis2 = pd.read_csv(open("squareroot-of-sum-of-squares-of-real-and-imaginary.csv"),)
+
+# TAB 1 Visualizations
+# ------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------
+p_v1 = figure(title="Real vs Imaginary parts of Eigen Values", width=vis_common_width, height=vis_common_height, tools=vis_common_tab_tools + ",lasso_select")
+p_v1.xaxis.axis_label = "Real"
+p_v1.yaxis.axis_label = "Imaginary"
+p_v1.circle(data_collection_vis1_formatted["x"], data_collection_vis1_formatted["y"], size=10)
+tab1 = Panel(child=p_v1, title="Visualization 1 - (x)+(y)j")
+
+# TAB 2 Visualizations
+# ------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------
+p_v2 = Line(data_collection_vis2, title="Magnitude vs. Eigen Position", xlabel='Position in Eigen Value Set', ylabel='Magnitude', width=vis_common_width, height=vis_common_height, legend=True, tools=vis_common_tab_tools + ", hover")
+tab2 = Panel(child=p_v2, title="Visualization 2 - sqrt((x)^2+(y)^2)")
+
+tabs = Tabs(tabs=[tab1, tab2])  # Set Tabs
+show(tabs)  # Show Visualization Output
+
